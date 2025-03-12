d002710609e7e72e2aa942c9d7f37f70c3bea1cb
Amina <95443359+AminaAsif9@users.noreply.github.com>
2025-03-12 09:39:05 +0500

    Time: 0 ms (100%), Space: 17.9 MB (17.73%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,20 +1,9 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        current_number = n
-        number = 0 # store the sum of the squares of the digits
-        numbers = {} # dictionary used to track numbers we’ve seen before.
-        
-        while True:
-            #This loop will keep running until we either reach 1 (happy number) 
-            #or detect a cycle (not a happy number).
+        seen = set()  
 
-            for i in str(current_number): #Convert current_number to a string so we can iterate over its digits.
-                number += int(i) ** 2
-            if number == 1:
-                return True
-            if number in numbers: #If number was seen before in numbers, we have a cycle, meaning n is not a happy number.
-                return False
-            numbers[number] = 0 # Store number in the dictionary to track it.
-            current_number = number 
-            number = 0
-#O(logn)
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = sum(int(digit) ** 2 for digit in str(n))  
+
+        return n == 1

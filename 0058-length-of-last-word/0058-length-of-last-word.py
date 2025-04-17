700b40c76ec7a332aede498d76c257dba521949a
Amina <95443359+AminaAsif9@users.noreply.github.com>
2025-04-17 15:00:59 +0500

    Update 0058-length-of-last-word.py

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,1 +1,16 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        # return len(s.strip().split()[-1])
 
+#  Other Approach
+        
+        length = 0
+
+        for i in s[::-1]:
+            if i == " ": # if there is space
+                if length >= 1:
+                    break
+            else: 
+                length += 1
+
+        return length

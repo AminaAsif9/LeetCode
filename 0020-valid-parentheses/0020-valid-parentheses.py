45e0cb10a9225bc8cdf39436eea0d91325ba8da1
Amina <95443359+AminaAsif9@users.noreply.github.com>
2025-08-29 17:29:01 +0500

    Time: 0 ms (100%), Space: 17.8 MB (81.18%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,12 +1,29 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = [] #A list is used as a stack to keep track of unmatched opening brackets.
-        brackets = {')': '(', ']': '[', '}': '{'} #A dictionary mapping each closing bracket to its corresponding opening bracket.
-        for c in s: #character c in string s
-            if c in "[{(": #if c is an opening bracket,
-                stack.append(c)
+        stack =[]
+        for c in s:
+            if c == "(":
+                stack.append(")")
+            elif c == "{":
+                stack.append("}")
+            elif c == "[":
+                stack.append("]")
             else:
-                if not stack or stack[-1] != brackets[c]:
+            # Check if stack is empty or if the top element doesn't match
+                if not stack or c != stack.pop():
                     return False
-                stack.pop()
+
+    # Check if the stack is empty at the end
         return not stack
+
+        #  other approach
+        # stack = [] #A list is used as a stack to keep track of unmatched opening brackets.
+        # brackets = {')': '(', ']': '[', '}': '{'} #A dictionary mapping each closing bracket to its corresponding opening bracket.
+        # for c in s: #character c in string s
+        #     if c in "[{(": #if c is an opening bracket,
+        #         stack.append(c)
+        #     else:
+        #         if not stack or stack[-1] != brackets[c]:
+        #             return False
+        #         stack.pop()
+        # return not stack

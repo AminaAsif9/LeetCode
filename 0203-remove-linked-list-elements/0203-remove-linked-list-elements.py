bc891e3914cf65c524b21d95c582382eeaca4f93
Amina <95443359+AminaAsif9@users.noreply.github.com>
2025-09-02 18:09:56 +0500

    Time: 1 ms (50.33%), Space: 19.7 MB (41.58%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -5,19 +5,22 @@
 #         self.next = next
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        if head == None:
+        if head == None: # base case
             return head
         
-        while head != None and head.val == val:
-            head = head.next
+        # checking head
+        while head != None and head.val == val: # run loop until head is not none or is equal to val
+            head = head.next # if head is equal to val then make head next value, head.
         if head == None:
-            return None
+            return None # as we want to remove
 
+        # checking other values, other than head
         temp = head
         while temp.next != None:
-            if temp.next.val != val:
+            if temp.next.val != val: # if temp next value not equal to val
                 temp = temp.next
             else:
-                temp.next = temp.next.next
+                temp.next = temp.next.next # pointing temp next to temp next next, 
+                # means in temp.next is now temp.next.next actually and it's skipping any pointer to next val which is 6
         
         return head

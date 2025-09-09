4e55163fe935f45aca35681eb9caf909095741c9
Amina <95443359+AminaAsif9@users.noreply.github.com>
2025-09-10 01:27:12 +0500

    Time: 0 ms (100%), Space: 17.8 MB (48.57%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -12,3 +12,20 @@ class Solution:
             first, second = second, third
 
         return second
+
+    # class Solution:
+    # def climbStairs(self, n: int) -> int:
+    #     """
+    #     (n-1) 
+    #     two variables  one, two
+    #     keep on updating then 
+    #     return one
+    #     """
+    #     one=1
+    #     two=1
+    #     for i in range(n-1):
+    #         temp=one
+    #         one=one+two
+    #         two=temp
+    #     return one
+        
